823d2eb32f35f588ab7fdf529c8398e087049b34
Anshu Rajpoot <107042248+Anshu-Rajpoot@users.noreply.github.com>
2023-03-12 00:04:14 +0530

    Add files via upload

---
+++ Prime Number.py
@@ -0,0 +1,34 @@
+Python 3.9.13 (tags/v3.9.13:6de2ca5, May 17 2022, 16:36:42) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=========================== RESTART: C:\Users\Anshu\Desktop\zz.py ==========================
+Enter the Number : 34
+34 Number is not Prime
+>>> 
+=========================== RESTART: C:\Users\Anshu\Desktop\zz.py ==========================
+Enter the Number : 43
+43 Number is Prime
+43 Number is Prime
+43 Number is Prime
+43 Number is Prime
+43 Number is Prime
+43 Number is Prime
+43 Number is Prime
+43 Number is Prime
+43 Number is Prime
+43 Number is Prime
+43 Number is Prime
+43 Number is Prime
+43 Number is Prime
+43 Number is Prime
+43 Number is Prime
+43 Number is Prime
+43 Number is Prime
+43 Number is Prime
+43 Number is Prime
+43 Number is Prime
+>>> 
+=========================== RESTART: C:\Users\Anshu\Desktop\zz.py ==========================
+Enter the Number : 43
+43 Number is Prime
+>>> 
